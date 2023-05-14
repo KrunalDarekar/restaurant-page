eb6449af43f1ee1a5c6f83ba524eca23fd989d4d
Krunal <96249206+KrunalDarekar@users.noreply.github.com>
2023-05-14 12:03:42 +0530

    Add the content in contact.js and style it

--- src/contact.js
+++ src/contact.js
@@ -1,8 +1,19 @@
+import './contact.css';
+
 const onContactClick = () => {
     const component = document.createElement('div');
     component.classList.add('contact-component');
 
-    component.textContent = "contact info here";
+    const call = document.createElement('div');
+    call.classList.add('call');
+    call.textContent = 'Call: 4888-4888'
+
+    const address = document.createElement('div');
+    address.classList.add('address');
+    address.textContent = 'Address: The pizzas are so good here you can only find this place in your dreams.';
+
+    component.appendChild(call);
+    component.appendChild(address);
 
     return component;
 }
