c0963a424b0e154ac675074e8e62b2422e2438cb
Krunal <96249206+KrunalDarekar@users.noreply.github.com>
2023-05-14 15:00:52 +0530

    Add the content for menu.js and style it

--- src/index.js
+++ src/index.js
@@ -36,6 +36,19 @@ menuBtn.onclick = () => {
     contactBtn.classList.remove('active');
 
     main.appendChild(onMenuClick());
+
+    const name1 = document.querySelector('.name1');
+    name1.textContent = 'Margherita';
+    const name2 = document.querySelector('.name2');
+    name2.textContent = 'Corn & Cheese';
+    const name3 = document.querySelector('.name3');
+    name3.textContent = 'Farmhouse';
+    const name4 = document.querySelector('.name4');
+    name4.textContent = 'Peppy Paneer';
+    const name5 = document.querySelector('.name5');
+    name5.textContent = 'BBQ Chicken';
+    const name6 = document.querySelector('.name6');
+    name6.textContent = 'Chicken Sausage';
 }
 
 contactBtn.onclick = () => {
--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,23 @@
+import './menu.css'
+
 const onMenuClick = () => {
     const component = document.createElement('div');
     component.classList.add('menu-component');
     
-    component.textContent = "here will be the menu";
+    for(let i = 1; i < 7; i++) {
+        const menuElement = document.createElement('div');
+        menuElement.classList.add(`element${i}`);
+
+        const img = document.createElement('div');
+        img.classList.add('img');
+        const name = document.createElement('div');
+        name.classList.add(`name${i}`);
+
+        menuElement.appendChild(img);
+        menuElement.appendChild(name);
+
+        component.appendChild(menuElement);
+    }
 
     return component;
 }
